7b1dc1ed26e29761d81dde18e8e7a7ccf3a7b131
KamilWloch <109965679+KamilWloch@users.noreply.github.com>
2023-06-27 22:24:54 +0200

    Update task-04.js
    Task 4 completed

---
+++ js/task-04.js
@@ -0,0 +1,23 @@
+let counterValue = 0;
+const ref = {
+    decrementBtn: document.querySelector('button[data-action="decrement"]'),
+    incrementBtn: document.querySelector('button[data-action="increment"]'),
+    value: document.querySelector('#value'),
+}
+
+ref.decrementBtn?.addEventListener("click", handleClickDecrement)
+ref.incrementBtnBtn?.addEventListener("click", handleClickIncrement)
+
+function setCounterValue(val) {
+    if (counterValue === val) return;
+    counterValue = val;
+    ref.value.textContent = String(val);
+}
+
+function handleClickDecrement() {
+    setCounterValue(counterValue - 1);
+}
+
+function handleClickIncrement() {
+    setCounterValue(counterValue + 1);
+}
