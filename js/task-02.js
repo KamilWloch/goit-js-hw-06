464202d4710b78657941b60a9ac371821eedfd1f
KamilWloch <109965679+KamilWloch@users.noreply.github.com>
2023-06-27 21:47:48 +0200

    Update task-02.js
    Task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,7 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingridientsNames = document.querySelector('ul#ingridients');
+const getLiNames = (ingridient) => `<li class="item">${ingridient}</li>`
+const getListNames = (ingridientsList) => ingridientsList.map(it => getLiNames(it)).join('')
+ingridientsNames.insertAdjacentHTML('afterbegin', getListNames(ingredients));
