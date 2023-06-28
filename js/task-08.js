e589c422f195cd235cb7df638dfe006279237bab
KamilWloch <109965679+KamilWloch@users.noreply.github.com>
2023-06-28 16:58:10 +0200

    Update task-08.js
    8 zadanie zrobione

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector('form.login-form');
 
+formRef.addEventListener('submit', handleSubmitForm)
+
+function handleSubmitForm(evt) {
+    evt.preventDefault();
+
+    const { email, password } = evt.currentTarget.elements;
+
+    if (email.value === '' || password.value === '') {
+        alert('Please fill all fields!');
+        return;
+    }
+
+    const formData = {
+        email: email.value,
+        password: password.value,
+    }
+
+    console.log(formData)
+    formRef.reset();
+}
