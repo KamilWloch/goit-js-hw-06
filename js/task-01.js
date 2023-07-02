1980a04ba7c35d0b541e4312aa4001fd6074e9b2
KamilWloch <109965679+KamilWloch@users.noreply.github.com>
2023-07-02 17:32:05 +0200

    Update task-01.js
    Typo + optimisation

--- js/task-01.js
+++ js/task-01.js
@@ -3,6 +3,6 @@ const categoryItems = categoriesReference?.querySelectorAll("li.item");
 console.log(`Number of categories: ${categoryItems.length}`);
 categoryItems.forEach(item => {
     const category = item.querySelector("h2").textContent;
-    const elementsCount = item.querySelectorAll(":scope > ul > li").lenght;
+    const elementsCount = item.querySelectorAll("li").length;
     console.log(`Category: ${category}/nElements: ${elementsCount}`)
 })
