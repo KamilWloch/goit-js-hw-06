3e5dea93786074b475fe3ffa15f8e2e9c9441168
KamilWloch <109965679+KamilWloch@users.noreply.github.com>
2023-07-02 17:46:48 +0200

    Update task-04.js
    Increase/decrease function corrections

--- js/task-04.js
+++ js/task-04.js
@@ -1,23 +1,30 @@
 let counterValue = 0;
-const ref = {
-    decrementBtn: document.querySelector('button[data-action="decrement"]'),
-    incrementBtn: document.querySelector('button[data-action="increment"]'),
-    value: document.querySelector('#value'),
-}
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const spanBtn = document.querySelector('#value');
 
-ref.decrementBtn?.addEventListener("click", handleClickDecrement)
-ref.incrementBtnBtn?.addEventListener("click", handleClickIncrement)
 
-function setCounterValue(val) {
-    if (counterValue === val) return;
-    counterValue = val;
-    ref.value.textContent = String(val);
-}
+decrementBtn.addEventListener("click", () => {
+    counterValue = counterValue - 1;
+    spanBtn.textContent = counterValue;
+});
 
-function handleClickDecrement() {
-    setCounterValue(counterValue - 1);
-}
+incrementBtn.addEventListener("click", () => {
+    counterValue = counterValue + 1;
+    spanBtn.textContent = counterValue;
+});
 
-function handleClickIncrement() {
-    setCounterValue(counterValue + 1);
-}
+
+// function setCounterValue(val) {
+//     if (counterValue === val) return;
+//     counterValue = val;
+//     ref.value.textContent = String(val);
+// }
+
+// function handleClickDecrement() {
+//     setCounterValue(counterValue - 1);
+// }
+
+// function handleClickIncrement() {
+//     setCounterValue(counterValue + 1);
+// }
